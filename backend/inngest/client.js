7d1274365cfc3207bc043981344846d44f1d06ae
Tarun Sharma <tarunsharma08.com@gmail.com>
2025-09-20 13:44:00 +0530

    updated event key for inngest

--- backend/inngest/client.js
+++ backend/inngest/client.js
@@ -1,5 +1,10 @@
 import { Inngest } from 'inngest'
+import dotenv from "dotenv"
+dotenv.config()
 
-export const inngest = new Inngest({id: "ticketing-system"})
+export const inngest = new Inngest({
+    id: "ticketing-system", 
+    eventKey: process.env.INNGEST_EVENT_KEY
+})
 
 export default inngest
